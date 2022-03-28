aa851376e772379e2e013c44241eec2dd8916469
Ashaun Thomas <ashaunthomas@siu.edu>
2022-03-27 19:25:52 -0700

    basic websocket server set up

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,39 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+//! Simple echo websocket server.
+//!
+//! Open `http://localhost:8080/` in browser to test.
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use actix_files::NamedFile;
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
+
+mod server;
+use self::server::MyWebSocket;
+
+async fn index() -> impl Responder {
+    NamedFile::open_async("../frontend/index.html").await.unwrap()
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+/// WebSocket handshake and start `MyWebSocket` actor.
+async fn echo_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    ws::start(MyWebSocket::new(), &req, stream)
 }
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            // WebSocket UI HTML file
+            .service(web::resource("/").to(index))
+            // websocket route
+            .service(web::resource("/ws").route(web::get().to(echo_ws)))
+            // enable logger
+            .wrap(middleware::Logger::default())
     })
+    .workers(2)
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
--- backend/target/debug/backend.d
+++ backend/target/debug/backend.d
@@ -1,1 +1,1 @@
-/Users/ashaun/Projects/websocket-tic-tac-toe/backend/target/debug/backend: /Users/ashaun/Projects/websocket-tic-tac-toe/backend/src/main.rs
+/Users/ashaun/Projects/websocket-tic-tac-toe/backend/target/debug/backend: /Users/ashaun/Projects/websocket-tic-tac-toe/backend/src/main.rs /Users/ashaun/Projects/websocket-tic-tac-toe/backend/src/server.rs
